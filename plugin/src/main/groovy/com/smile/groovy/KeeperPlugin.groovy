81a009a6336ebc419808a71201285fb933329a9c
Smile <lijianhy1990@gmail.com>
2021-03-11 19:51:39 +0800

    first commit

---
+++ plugin/src/main/groovy/com/smile/groovy/KeeperPlugin.groovy
@@ -0,0 +1,46 @@
+package com.smile.groovy
+
+
+import com.android.build.gradle.LibraryExtension
+import com.android.build.gradle.api.LibraryVariant
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.logging.Logger
+import org.gradle.api.tasks.TaskProvider
+import proguard.obfuscate.MappingReader
+
+class KeeperPlugin implements Plugin<Project> {
+    Project project
+    static Logger logger
+
+    @Override
+    void apply(Project target) {
+        project = target
+        logger = project.logger
+        LibraryExtension libraryExtension = project.extensions.findByType(LibraryExtension.class)
+        def taskContainer = project.tasks
+        project.afterEvaluate {
+            libraryExtension.libraryVariants.each { LibraryVariant variant ->
+                if (!variant.buildType.minifyEnabled) {
+                    return
+                }
+                variant.packageLibraryProvider.get().doFirst {
+                    String mergeTaskName = "merge${variant.name.capitalize()}ConsumerProguardFiles"
+                    TaskProvider mergeFileTask = taskContainer.named(mergeTaskName)
+                    if (mergeFileTask == null) {
+                        throw new RuntimeException("Can not find task ${mergeTaskName}!")
+                    }
+                    // todo need to adapt other gradle version
+                    def mergeProguardFile = mergeFileTask.get().outputs.files.singleFile
+                    def mappingFile = variant.getMappingFileProvider().get().singleFile
+
+                    def mappingReader = new MappingReader(mappingFile)
+                    def mappingProcess = new SelfMappingProcess(mergeProguardFile)
+                    mappingReader.pump(mappingProcess)
+                    mappingProcess.finish()
+                }
+            }
+        }
+    }
+
+}
---
+++ plugin/src/main/groovy/com/smile/groovy/SelfMappingProcess.groovy
@@ -0,0 +1,39 @@
+package com.smile.groovy
+
+import proguard.obfuscate.MappingProcessor
+
+class SelfMappingProcess implements MappingProcessor {
+
+    PrintWriter printWriter
+
+    SelfMappingProcess(File targetFile) {
+        printWriter = new PrintWriter(new BufferedWriter(new OutputStreamWriter(
+                new FileOutputStream(targetFile, true))), false)
+        printWriter.println()
+        printWriter.println()
+        printWriter.println('# this is mapping keeper print begin --')
+        printWriter.println()
+    }
+
+    @Override
+    boolean processClassMapping(String className, String newClassName) {
+        printWriter.println("-keepnames class ${newClassName} {*;}")
+        return false
+    }
+
+    @Override
+    void processFieldMapping(String className, String fieldType, String fieldName, String newClassName, String newFieldName) {
+    }
+
+    @Override
+    void processMethodMapping(String className, int firstLineNumber, int lastLineNumber, String methodReturnType, String methodName, String methodArguments, String newClassName, int newFirstLineNumber, int newLastLineNumber, String newMethodName) {
+
+    }
+
+    void finish() {
+        printWriter.println('# this is mapping keeper print end --')
+        printWriter.println()
+        printWriter?.flush()
+        printWriter?.close()
+    }
+}
